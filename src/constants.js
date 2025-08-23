1cec312899bf8a0165143743986cd88a609cfd1d
PranavArora20 <arorapranav73@gmail.com>
2025-08-24 00:28:16 +0530

    updated skills part

--- src/constants.js
+++ src/constants.js
@@ -1,13 +1,50 @@
 
+// Skills Section Logo's
+import htmlLogo from "./assets/tech_logo/html.png";
+import cssLogo from "./assets/tech_logo/css.png";
+import sassLogo from "./assets/tech_logo/sass.png";
+import javascriptLogo from "./assets/tech_logo/javascript.png";
+import reactjsLogo from "./assets/tech_logo/reactjs.png";
+import angularLogo from "./assets/tech_logo/angular.png";
+import reduxLogo from "./assets/tech_logo/redux.png";
+import nextjsLogo from "./assets/tech_logo/nextjs.png";
+import tailwindcssLogo from "./assets/tech_logo/tailwindcss.png";
+import gsapLogo from "./assets/tech_logo/gsap.png";
+import materialuiLogo from "./assets/tech_logo/materialui.png";
+import bootstrapLogo from "./assets/tech_logo/bootstrap.png";
+import springbootLogo from "./assets/tech_logo/springboot.png";
+import nodejsLogo from "./assets/tech_logo/nodejs.png";
+import expressjsLogo from "./assets/tech_logo/express.png";
+import mysqlLogo from "./assets/tech_logo/mysql.png";
+import mongodbLogo from "./assets/tech_logo/mongodb.png";
+import firebaseLogo from "./assets/tech_logo/firebase.png";
+import cLogo from "./assets/tech_logo/c.png";
+import cppLogo from "./assets/tech_logo/cpp.png";
+import javaLogo from "./assets/tech_logo/java.png";
+import pythonLogo from "./assets/tech_logo/python.png";
+import typescriptLogo from "./assets/tech_logo/typescript.png";
+import gitLogo from "./assets/tech_logo/git.png";
+import githubLogo from "./assets/tech_logo/github.png";
+import vscodeLogo from "./assets/tech_logo/vscode.png";
+import postmanLogo from "./assets/tech_logo/postman.png";
+import mcLogo from "./assets/tech_logo/mc.png";
+import figmaLogo from "./assets/tech_logo/figma.png";
+import netlifyLogo from "./assets/tech_logo/netlify.png";
+import vercelLogo from "./assets/tech_logo/vercel.png";
+import postgreLogo from "./assets/tech_logo/postgre.png";
+import csharpLogo from "./assets/tech_logo/csharp.png";
+
 // Experience Section Logo's
 import webverseLogo from "./assets/company_logo/webverse_logo.png";
 import agcLogo from "./assets/company_logo/agc_logo.png";
 import newtonschoolLogo from "./assets/company_logo/newtonschool_logo.png";
 
 // Education Section Logo's
+import glaLogo from "./assets/education_logo/gla_logo.png";
+import bsaLogo from "./assets/education_logo/bsa_logo.png";
+import vpsLogo from "./assets/education_logo/vps_logo.png";
 import adgips from "./assets/education_logo/adgips.png";
 import apj from "./assets/education_logo/apj.png";
-import apj2 from "./assets/education_logo/apj.png";
 
 // Project Section Logo's
 import assetvue from "./assets/work_logo/AssetVue.png";
@@ -24,46 +61,46 @@ export const SkillsInfo = [
   {
     title: "Frontend",
     skills: [
-      { name: "HTML", logo: "/src/assets/tech_logo/html.png" },
-      { name: "CSS", logo: "/src/assets/tech_logo/css.png" },
-      { name: "JavaScript", logo: "/src/assets/tech_logo/javascript.png" },
-      { name: "React JS", logo: "/src/assets/tech_logo/reactjs.png" },
-      { name: "Redux", logo: "/src/assets/tech_logo/redux.png" },
-      // { name: "Next JS", logo: "/src/assets/tech_logo/nextjs.png" },
-      { name: "Tailwind CSS", logo: "/src/assets/tech_logo/tailwindcss.png" },
-      { name: "Material UI", logo: "/src/assets/tech_logo/materialui.png" },
-      { name: "Bootstrap", logo: "/src/assets/tech_logo/bootstrap.png" },
+      { name: "HTML", logo: htmlLogo },
+      { name: "CSS", logo: cssLogo },
+      { name: "JavaScript", logo: javascriptLogo },
+      { name: "React JS", logo: reactjsLogo },
+      { name: "Redux", logo: reduxLogo },
+      // { name: "Next JS", logo: nextjsLogo },
+      { name: "Tailwind CSS", logo: tailwindcssLogo },
+      { name: "Material UI", logo: materialuiLogo },
+      { name: "Bootstrap", logo: bootstrapLogo },
     ],
   },
   {
     title: "Backend",
     skills: [
-      { name: "Node JS", logo: "/src/assets/tech_logo/nodejs.png" },
-      { name: "Express JS", logo: "/src/assets/tech_logo/express.png" },
-      { name: "MySQL", logo: "/src/assets/tech_logo/mysql.png" },
-      { name: "MongoDB", logo: "/src/assets/tech_logo/mongodb.png" },
-      { name: "Firebase", logo: "/src/assets/tech_logo/firebase.png" },
+      { name: "Node JS", logo: nodejsLogo },
+      { name: "Express JS", logo: expressjsLogo },
+      { name: "MySQL", logo: mysqlLogo },
+      { name: "MongoDB", logo: mongodbLogo },
+      { name: "Firebase", logo: firebaseLogo },
     ],
   },
   {
     title: "Languages",
     skills: [
-      { name: "Java", logo: "/src/assets/tech_logo/java.png" },
-      { name: "JavaScript", logo: "/src/assets/tech_logo/javascript.png" },
-      { name: "TypeScript", logo: "/src/assets/tech_logo/typescript.png" },
+      { name: "Java", logo: javaLogo },
+      { name: "JavaScript", logo: javascriptLogo },
+      { name: "TypeScript", logo: typescriptLogo },
     ],
   },
   {
     title: "Tools",
     skills: [
-      { name: "Git", logo: "/src/assets/tech_logo/git.png" },
-      { name: "GitHub", logo: "/src/assets/tech_logo/github.png" },
-      { name: "VS Code", logo: "/src/assets/tech_logo/vscode.png" },
-      { name: "Postman", logo: "/src/assets/tech_logo/postman.png" },
-      { name: "Compass", logo: "/src/assets/tech_logo/mc.png" },
-      { name: "Vercel", logo: "/src/assets/tech_logo/vercel.png" },
-      { name: "Netlify", logo: "/src/assets/tech_logo/netlify.png" },
-      { name: "Figma", logo: "/src/assets/tech_logo/figma.png" },
+      { name: "Git", logo: gitLogo },
+      { name: "GitHub", logo: githubLogo },
+      { name: "VS Code", logo: vscodeLogo },
+      { name: "Postman", logo: postmanLogo },
+      { name: "Compass", logo: mcLogo },
+      { name: "Vercel", logo: vercelLogo },
+      { name: "Netlify", logo: netlifyLogo },
+      { name: "Figma", logo: figmaLogo },
     ],
   },
 ];
@@ -135,16 +172,16 @@ export const education = [
     grade: "82.6%",
     desc: "I completed my Class 12 education from Apeejay School, Pitampura, Delhi under the CBSE board, with a focus on Physics, Chemistry, Mathematics (PCM), and Information Practices. My senior secondary education helped me build strong analytical and logical thinking skills, which laid the foundation for my technical journey.",
     degree: "CBSE (XII) - PCM with Information Practices",
-},
-{
+  },
+  {
     id: 2,
-    img: apj2,
+    img: apj,
     school: "Apeejay School Pitampura, Delhi",
     date: "Apr 2018 - May 2019",
     grade: "86.2%",
     desc: "I completed my Class 10 education from Apeejay School, Pitampura, Delhi under the CBSE board, with a focus on Science and Computer Applications. My secondary education gave me an early interest in technology and problem-solving, which motivated me to pursue a career in the IT field.",
     degree: "CBSE (X) - Science with Computer Applications",
-}
+  }
 ];
 
 
